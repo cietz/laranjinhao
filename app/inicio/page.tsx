a3e651e73110113304c66dd48abc348c13628c10
Caio <caiolvg@gmail.com>
2025-12-12 13:33:41 -0300

    Atualiza preço do plano anual para R$ 19,90

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -482,7 +482,7 @@ export default function LaranjinhaMidiasPage() {
               <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
               <path d="M12 18V6" />
             </svg>
-            Plano Anual R$ 5,00
+            Plano Anual R$ 19,90
           </button>
 
           {/* =============================================================== */}
